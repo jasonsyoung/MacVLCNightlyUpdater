e2f58968a3fd95d3391e3205c41307e1a3ebd9c3
Jason Young <jasonsyoung>
2019-01-30 11:14:18 -0800

    Fixing notifications

--- bin/vlc-nightly-updater.sh
+++ bin/vlc-nightly-updater.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
-pushd $(dirname "$(\realpath "$0")") &>/dev/null
-npm start
+echo "var app = Application.currentApplication();\
+app.includeStandardAdditions = true;\
+app.displayNotification('VLC Nighly Updater', {\
+    withTitle: 'Update started...'\
+});" | osascript -s he -l JavaScript - 
+export PATH="/usr/local/bin:$PATH"
+pushd $(/usr/bin/dirname "$(/usr/local/bin/realpath "$0")") &>/dev/null
+/usr/local/bin/npm start
 popd &>/dev/null
+
+echo "var app = Application.currentApplication();\
+app.includeStandardAdditions = true;\
+app.displayNotification('VLC Nighly Updater', {\
+    withTitle: 'Install completed...'\
+});" | osascript -s he -l JavaScript - 
