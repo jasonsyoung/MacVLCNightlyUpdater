60a89f3d4955645f4960f8b785acfae05456f642
Jason Young <jasonsyoung>
2019-02-03 08:49:20 -0800

    Fixing bin

--- vlc-nightly-updater.sh
+++ vlc-nightly-updater.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
-const realPath = fs.realpathSync(__filename)
-const realLink = fs.readlinkSync(realPath)
-const projPath = path.resolve('..', realLink)
-const program = path.join(projPath, 'index.js')
+if (fs.statSync(__filename).isSymbolicLink()) {
+    const realPath = fs.readlinkSync(__filename)
+    const realLink = fs.realpathSync(realPath)
+    const projPath = path.resolve('..', realLink)
+    const program = path.join(projPath, 'index.js')
 
-require(program)(/-?-d(aemon)?/.test(process.argv[2], process.argv[3]))
+    require(program)(/-?-d(aemon)?/.test(process.argv[2], process.argv[3]))
+} else {
+    const realPath = fs.realpathSync(__filename)
+    const program = path.join(realPath, '..', 'index.js')
+
+    require(program)(/-?-d(aemon)?/.test(process.argv[2], process.argv[3]))
+}
